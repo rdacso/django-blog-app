0d3c884badb852f3d2ce699ad5f2ad786f162b9b
Rebecca Dacso <rdacso@Rebeccas-MacBook-Pro.local>
2017-05-26 14:47:56 -0700

    models in progress

---
+++ suorganizer/organizer/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Model Field Reference
+
+class Tag(models.Model):
+    name = models.CharField(max_length=31, unique=True)
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config.')
+
+
+class Startup(models.Model):
+    name = models.CharField(max_length=31, db_index=True)
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config.')
+    description = models.TextField()
+    founded_date = models.DateField('date founded')
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=63)
+    pub_date = models.DateField('date published')
+    link = models.URLField(max_length=255)
+    startup = models.ForeignKey(Startup)

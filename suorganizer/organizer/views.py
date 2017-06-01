2385d154bb7dfc58bcad47ff0049a032ae3c02d5
Rebecca Dacso <rdacso@Rebeccas-MacBook-Pro.local>
2017-06-01 14:37:10 -0700

    added templates to blog and organizer apps and root directory

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from .models import Tag
 
-# Create your views here.
+def homepage(request):
+    tag_list = Tag.objects.all()
+    output = ', '.join([tag.name for tag in tag_list])
+    return HttpResponse(output)
--- suorganizer/suorganizer/settings.py
+++ suorganizer/suorganizer/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'organizer',
     'blog',
+
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,8 @@ ROOT_URLCONF = 'suorganizer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

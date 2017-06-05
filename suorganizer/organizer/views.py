c671e231d5219d0f3298ac5145f98e25c62a6c78
Rebecca Dacso <rdacso@Rebeccas-MacBook-Pro.local>
2017-06-05 12:44:50 -0700

    started working on organizer app views

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.template import Context, loader
 from .models import Tag
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = ', '.join([tag.name for tag in tag_list])
+    template = loader.get_template('organizer/tag_list.html')
+    context = {'tag_list':tag_list}
+    output = template.render(context)
     return HttpResponse(output)
